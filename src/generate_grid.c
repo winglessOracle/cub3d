5f992af8c8571181479f06dffa4571d0de0ac222
winglessOracle <carlo@wesseling.io>
2023-08-22 16:53:48 +0200

    norminette on grid functions (added helper)

--- src/generate_grid.c
+++ src/generate_grid.c
@@ -6,7 +6,7 @@
 /*   By: carlowesseling <carlowesseling@student.      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/22 14:33:39 by carlowessel   #+#    #+#                 */
-/*   Updated: 2023/08/22 16:21:04 by carlowessel   ########   odam.nl         */
+/*   Updated: 2023/08/22 16:53:27 by carlowessel   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,15 @@ bool	find_start_grid(char *line)
 	return (false);
 }
 
-char	**generate_grid(char *input_file)
+char	*grid_helper(int fd)
 {
-	int		fd;
 	char	*line;
 	char	*joined_lines;
-	char	**ret;
 	bool	start_grid;
 
 	line = "";
 	joined_lines = ft_strdup("");
 	start_grid = false;
-	fd = open(input_file, O_RDONLY);
-	if (fd < 0)
-		return (NULL);
 	while (line)
 	{
 		line = get_next_line(fd);
@@ -50,10 +45,24 @@ char	**generate_grid(char *input_file)
 			joined_lines = ft_strjoin_free(joined_lines, line);
 		}
 	}
-	close(fd);
+	close (fd);
 	if (joined_lines[0] == '\0' || joined_lines[0] == '\n')
 		return (free(joined_lines), NULL);
+	return (joined_lines);
+}
+
+
+char	**generate_grid(char *input_file)
+{
+	int		fd;
+	char	*joined_lines;
+	char	**ret;
+
+	fd = open(input_file, O_RDONLY);
+	if (fd < 0)
+		return (NULL);
+	joined_lines = grid_helper(fd);
 	ret = ft_split(joined_lines, '\n');
-	free (joined_lines);
+	free(joined_lines);
 	return (ret);
 }
