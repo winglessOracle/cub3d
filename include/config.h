63b45f26c151a22898af433ec1bf02a83a11b869
winglessOracle <carlo@wesseling.io>
2023-09-11 23:08:23 +0200

    updated and added textures and maps

--- include/config.h
+++ include/config.h
@@ -6,7 +6,7 @@
 /*   By: cwesseli <cwesseli@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/11 15:20:29 by cwesseli      #+#    #+#                 */
-/*   Updated: 2023/09/13 00:50:35 by carlowessel   ########   odam.nl         */
+/*   Updated: 2023/09/13 00:50:56 by carlowessel   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 # define CONFIG_H
 
 //dimensions of the MLX screenthe game will play on
-# define SCREEN_WIDTH 2200
-# define SCREEN_HEIGHT 1300
+# define SCREEN_WIDTH 1200
+# define SCREEN_HEIGHT 800
 
 # define WINDOW_X_POS 10
 # define WINDOW_Y_POS 10
