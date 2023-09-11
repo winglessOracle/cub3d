66f008862dd6dbc90283fb325213d19044ee13e2
winglessOracle <carlo@wesseling.io>
2023-09-11 18:36:32 +0200

    fixed mirror l r

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: cwesseli <cwesseli@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/21 14:25:00 by cwesseli      #+#    #+#                 */
-/*   Updated: 2023/09/11 15:42:57 by cwesseli      ########   odam.nl         */
+/*   Updated: 2023/09/11 18:29:14 by carlowessel   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
