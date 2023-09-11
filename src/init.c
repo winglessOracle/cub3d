8b004c3609546d343fc8edd7d52ca9a45d6ba203
winglessOracle <carlo@wesseling.io>
2023-09-11 22:20:25 +0200

    performance gain by better use of MLX lib fies

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: cwesseli <cwesseli@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/21 14:25:00 by cwesseli      #+#    #+#                 */
-/*   Updated: 2023/09/13 00:53:20 by carlowessel   ########   odam.nl         */
+/*   Updated: 2023/09/13 00:53:43 by carlowessel   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
