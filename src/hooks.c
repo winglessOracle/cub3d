0362b49db65fa304f32ec5e428a67cc506fb6aba
winglessOracle <carlo@wesseling.io>
2023-09-12 14:33:21 +0200

    fixed radar

--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: carlowesseling <carlowesseling@student.      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/23 14:43:27 by carlowessel   #+#    #+#                 */
-/*   Updated: 2023/09/13 00:25:53 by carlowessel   ########   odam.nl         */
+/*   Updated: 2023/09/13 00:28:35 by carlowessel   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
