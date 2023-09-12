fa591656033a8e7aedd6a19999dc9ad13526cf9f
winglessOracle <carlo@wesseling.io>
2023-09-12 20:25:51 +0200

    fixed mouse toggle

--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: carlowesseling <carlowesseling@student.      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/23 14:43:27 by carlowessel   #+#    #+#                 */
-/*   Updated: 2023/09/13 00:28:55 by carlowessel   ########   odam.nl         */
+/*   Updated: 2023/09/13 00:29:22 by carlowessel   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
