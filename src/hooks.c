957b1f9f24648d5844de22ff8f23ad8515472d79
winglessOracle <carlo@wesseling.io>
2023-09-12 14:37:27 +0200

    commented out release button keystrikes. seems to still work nice

--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: carlowesseling <carlowesseling@student.      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/23 14:43:27 by carlowessel   #+#    #+#                 */
-/*   Updated: 2023/09/13 00:53:54 by carlowessel   ########   odam.nl         */
+/*   Updated: 2023/09/13 00:54:05 by carlowessel   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
