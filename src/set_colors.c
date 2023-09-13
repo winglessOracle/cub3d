4e6617c3b5a6e8abebef24a7f10d365c2ea05c3b
winglessOracle <carlo@wesseling.io>
2023-09-13 16:42:58 +0200

    improved and refactores color setting and texture setting function. much less code and more save

--- src/set_colors.c
+++ src/set_colors.c
@@ -6,7 +6,7 @@
 /*   By: carlowesseling <carlowesseling@student.      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/22 14:35:29 by carlowessel   #+#    #+#                 */
-/*   Updated: 2023/09/13 12:11:03 by carlowessel   ########   odam.nl         */
+/*   Updated: 2023/09/13 16:13:47 by carlowessel   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,23 +31,26 @@ void	set_rgb(char *color_char, t_color *color, t_data *data)
 	int		i;
 	int		start;
 	char	temp[4];
+	int		counter;
 
 	i = 0;
-	start = 0;
-	while (color_char[i] && color_char[i] != ',')
-		i++;
-	ft_strlcpy(temp, color_char + start, i - start + 1);
-	color->red = ft_atoi(temp);
-	start = ++i;
-	while (color_char[i] && color_char[i] != ',')
-		i++;
-	ft_strlcpy(temp, color_char + start, i - start + 1);
-	color->green = ft_atoi(temp);
-	start = ++i;
-	while (color_char[i] && color_char[i] != ',')
-		i++;
-	ft_strlcpy(temp, color_char + start, i - start + 1);
-	color->blue = ft_atoi(temp);
+	counter = 0;
+	while (counter < 3)
+	{
+		while (!ft_isdigit(color_char[i]))
+			i++;
+		start = i;
+		while (color_char[i] && ft_isdigit(color_char[i]))
+			i++;
+		ft_strlcpy(temp, color_char + start, i++ - start + 1);
+		if (counter == 0)
+			color->red = ft_atoi(temp);
+		else if (counter == 1)
+			color->green = ft_atoi(temp);
+		else if (counter == 2)
+			color->blue = ft_atoi(temp);
+		counter++;
+	}
 	check_rgb_value(color, data);
 	color->argb = get_rgba(color->red, color->green, color->blue, color->a);
 }
@@ -67,6 +70,8 @@ void	set_color(char *line, char *identifier, t_color *col, t_data *data)
 			size++;
 		color = ft_substr(ident, 0, size);
 		set_rgb(color, col, data);
+		check_rgb_value(col, data);
+		col->argb = get_rgba(col->red, col->green, col->blue, col->a);
 		data->check_data->colors_loaded += 1;
 		free (color);
 	}
--- src/set_paths.c
+++ src/set_paths.c
@@ -6,7 +6,7 @@
 /*   By: carlowesseling <carlowesseling@student.      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/22 13:20:36 by carlowessel   #+#    #+#                 */
-/*   Updated: 2023/09/13 11:41:51 by carlowessel   ########   odam.nl         */
+/*   Updated: 2023/09/13 16:39:59 by carlowessel   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,137 +26,31 @@ void	check_and_load(t_texture *paths, int i, t_data *data)
 
 void	load_textures(char *line, t_data *data)
 {
-	int					size;
 	char				*ident;
+	char				*end;
 	int					i;
 	static t_texture	paths[] = {{"NO", NORTH}, {"EA", EAST},
 	{"SO", SOUTH}, {"WE", WEST}};
 
 	i = 0;
-	size = 0;
 	while (i < 4)
 	{
 		ident = ft_strnstr(line, paths[i].prefix, ft_strlen(line));
 		if (ident)
 		{
-			while (ident && *ident != '/')
+			while (*ident && *ident != '/')
 				ident++;
 			if (*ident == '/')
-				ident++;
-			while (ident[size] && !ft_isspace(ident[size]))
-				size++;
+				end = ++ident;
+			while (*end && !ft_isspace(*end))
+				end++;
 			data->img_data->wall_texture_paths[paths[i].dir]
-				= ft_substr(ident, 0, size);
+				= ft_substr(ident, 0, end - ident);
 			check_and_load(paths, i, data);
 		}
 		i++;
 	}
 }
-
-// void	load_north(char *line, t_data *data)
-// {
-// 	int		size;
-// 	char	*ident;
-
-// 	size = 0;
-// 	ident = ft_strnstr(line, "NO", ft_strlen(line));
-// 	if (ident)
-// 	{
-// 		while (ident && *ident != '/')
-// 			ident++;
-// 		if (*ident == '/')
-// 			ident++;
-// 		while (ident[size] && !ft_isspace(ident[size]))
-// 			size++;
-// 		data->img_data->wall_texture_paths[NORTH] = ft_substr(ident, 0, size);
-// 		if (ft_check_extention(data->img_data->wall_texture_paths[NORTH],"png"))
-// 			free_str_exit("wrong file format. expected '.png'", data, 7);
-// 		data->img_data->wall_textures[NORTH]
-// 			= mlx_load_png(data->img_data->wall_texture_paths[NORTH]);
-// 		if (!data->img_data->wall_textures[NORTH])
-// 			free_str_exit("loading texture", data, 6);
-// 		data->check_data->textures_loaded += 1;
-// 	}
-// }
-
-// void	load_east(char *line, t_data *data)
-// {
-// 	int		size;
-// 	char	*ident;
-
-// 	size = 0;
-// 	ident = ft_strnstr(line, "EA", ft_strlen(line));
-// 	if (ident)
-// 	{
-// 		while (ident && *ident != '/')
-// 			ident++;
-// 		if (*ident == '/')
-// 			ident++;
-// 		while (ident[size] && !ft_isspace(ident[size]))
-// 			size++;
-// 		data->img_data->wall_texture_paths[EAST] = ft_substr(ident, 0, size);
-// 		if (ft_check_extention(data->img_data->wall_texture_paths[EAST], "png"))
-// 			free_str_exit("wrong file format. expected '.png'", data, 7);
-// 		data->img_data->wall_textures[EAST]
-// 			= mlx_load_png(data->img_data->wall_texture_paths[EAST]);
-// 		if (!data->img_data->wall_textures[EAST])
-// 			free_str_exit("loading texture", data, 6);
-// 		data->check_data->textures_loaded += 1;
-// 	}
-// }
-
-// void	load_south(char *line, t_data *data)
-// {
-// 	int		size;
-// 	char	*ident;
-
-// 	size = 0;
-// 	ident = ft_strnstr(line, "SO", ft_strlen(line));
-// 	if (ident)
-// 	{
-// 		while (ident && *ident != '/')
-// 			ident++;
-// 		if (*ident == '/')
-// 			ident++;
-// 		while (ident[size] && !ft_isspace(ident[size]))
-// 			size++;
-// 		data->img_data->wall_texture_paths[SOUTH] = ft_substr(ident, 0, size);
-// 		if (ft_check_extention
-// 			(data->img_data->wall_texture_paths[SOUTH], "png"))
-// 			free_str_exit("wrong file format. expected '.png'",	data, 7);
-// 		data->img_data->wall_textures[SOUTH]
-// 			= mlx_load_png(data->img_data->wall_texture_paths[SOUTH]);
-// 		if (!data->img_data->wall_textures[SOUTH])
-// 			free_str_exit("loading texture", data, 6);
-// 		data->check_data->textures_loaded += 1;
-// 	}
-// }
-
-// void	load_west(char *line, t_data *data)
-// {
-// 	int		size;
-// 	char	*ident;
-
-// 	size = 0;
-// 	ident = ft_strnstr(line, "WE", ft_strlen(line));
-// 	if (ident)
-// 	{
-// 		while (ident && *ident != '/')
-// 			ident++;
-// 		if (*ident == '/')
-// 			ident++;
-// 		while (ident[size] && !ft_isspace(ident[size]))
-// 			size++;
-// 		data->img_data->wall_texture_paths[WEST] = ft_substr(ident, 0, size);
-// 		if (ft_check_extention(data->img_data->wall_texture_paths[WEST], "png"))
-// 			free_str_exit("wrong file format. expected '.png'",	data, 7);
-// 		data->img_data->wall_textures[WEST]
-// 			= mlx_load_png(data->img_data->wall_texture_paths[WEST]);
-// 		if (!data->img_data->wall_textures[WEST])
-// 			free_str_exit("loading texture", data, 6);
-// 		data->check_data->textures_loaded += 1;
-// 	}
-// }
 
 /***
  * @brief checks every line of he input file
@@ -178,10 +72,6 @@ void	parse_file_paths(char *file, t_data *data)
 		if (line == NULL)
 			break ;
 		load_textures(line, data);
-		// load_north(line, data);
-		// load_east(line, data);
-		// load_south(line, data);
-		// load_west(line, data);
 		set_color(line, "C ", data->img_data->ceiling, data);
 		set_color(line, "F ", data->img_data->floor, data);
 		free (line);
